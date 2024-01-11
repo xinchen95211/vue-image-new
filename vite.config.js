05884fa94eead695ff1fe48e45413304b3d9b691
xinchen95211 <76147759+xinchen95211@users.noreply.github.com>
2024-01-11 09:51:04 +0800

    删除lock

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       '/api': {
         target: 'https://frp-hat.top:49728',
         // target:'http://192.168.0.108:9999',
+        secure:false,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
