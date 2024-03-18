7066d7f6abe4c9bcc77d84dabdd7a26ed1fff200
xinchen95211 <76147759+xinchen95211@users.noreply.github.com>
2024-03-18 11:50:07 +0800

    修复了一些bug

--- src/views/RandomVideo.vue
+++ src/views/RandomVideo.vue
@@ -1,7 +1,11 @@
 <template>
-  <div style="position: relative;z-index: -999; ">
+  <div style="position: relative; margin-top: 10px" >
     <vue3VideoPlay id="vueVideo" v-bind="options" :poster="poster" @error="errorLoad"/>
   </div>
+  <div style="display: flex;justify-content: center;">
+    <el-button type="primary" @click="mount" >点击换一个</el-button>
+  </div>
+
 <!--  <div class="zan">-->
 <!--    <el-icon size="50px" color="skyblue" @click="selectStar(id)">-->
 <!--      <star v-if="star"></star>-->
@@ -21,6 +25,7 @@ export default {
   components: {Vue3VideoPlay},
   data(){
     return{
+      onclicks:false,
       options:{
         width: '800px', //播放器高度
         height: '450px', //播放器高度
@@ -28,9 +33,8 @@ export default {
         speed:true, //关闭进度条拖动
         title: '', //视频名称
         pageFullScreen:false,
-        webFullScreen:true,
         fullscreen:true,
-        src:"",
+        src: "https://alimov2.a.kwimgs.com/upic/2023/08/03/16/BMjAyMzA4MDMxNjQ5MThfMjg4Mzc1MTA4OF8xMDk1ODM0NDMwMzRfMl8z_b_B7ef17b36344f6d9bf2d9e78b40abd1a6.mp4?clientCacheKey=3xygkip62k7bpci_b.mp4&tt=b&di=b72e16c7&bp=13414",
         controlBtns:['audioTrack', 'quality', 'speedRate', 'volume', 'setting', 'pip', 'fullScreen']
       },
       poster:'',
@@ -40,10 +44,24 @@ export default {
     }
   },
   created() {
-    this.mount()
+    this.$nextTick(() => {
+      let docHeight = document.documentElement.clientHeight;
+      let docwidth = document.documentElement.clientWidth;
+      console.log("可视区域高度", docHeight);
+      this.options.width = docwidth + "px";
+      this.options.height = docHeight * 0.90 + "px";
+      // this.tabHeight = docHeight * 0.14 + 'px';
+      // this.folderHeight = docHeight * 0.05 + 'px';
+      console.log(this.cardHeight, this.tabHeight, this.folderHeight)
+    });
+    this.mount();
   },
   methods:{
     mount() {
+      if (this.onclicks){
+        return;
+      }
+      this.onclicks = true;
       axios.get(`${this.$domainUrl}/video/random`).then(e => {
         if (e.data.code === 200) {
           let data = e.data.data;
@@ -52,7 +70,9 @@ export default {
           this.options.src = data.domain + data.videoUri;
           this.poster = data.domain + '/' + data.prefix + '/' +data.suffix + '/' +data.thumbnail;
         }
+        this.onclicks = false
       }).catch(error => {
+        this.onclicks = false
             console.log("error" + error)
           }
       )
@@ -85,6 +105,7 @@ export default {
       })
     },
     errorLoad(){
+
       if (this.options.src === ''){return;}
 
       if (this.iterateeCount === this.domainList.length){
@@ -98,10 +119,15 @@ export default {
   }
 </script>
 
-<style scoped>
+<style>
 .zan{
   float: right;
   margin-top:500px;
   margin-right: 10px;
+}
+html{
+  height: 100%;
+  width: 100%;
+  background-image: linear-gradient(to right, #FFFFFF,#FFFFFF);
 }
 </style>
