1d24e2b8cc984e9e9f2c0ec7dbd279669d8bfda9
xinchen95211 <76147759+xinchen95211@users.noreply.github.com>
2024-06-09 14:07:52 +0800

    添加随机视频的预加载功能

---
+++ src/views/NewPhotoMainView.vue
@@ -0,0 +1,44 @@
+<script>
+import axios from "axios";
+import WaterFall from "@/components/WaterFall.vue";
+
+export default {
+  data(){
+    return{
+      getValuePrefix:"photo_",
+    }
+  },
+  mounted() {
+   // this.down();
+  },
+
+  methods:{
+    selectItem(id,superId){
+      window.open('/#/photo/'+ id + '/'+ superId);
+    },
+    getPhotoValue(value,superValue,index){
+      let newValue = {};
+      newValue.id = index;
+      newValue.superId = superValue;
+      newValue.url = value.prefix + '/' + value.suffix + '/' + value.thumbnail;
+      return newValue;
+    },
+
+
+  },
+  props:{
+
+  },
+  components:{
+    WaterFall
+  }
+}
+</script>
+
+<template>
+<water-fall :getValuePrefix="getValuePrefix" :getPhotoValue="getPhotoValue" :selectItem="selectItem" :maxValue="296"></water-fall>
+</template>
+
+<style scoped>
+
+</style>
